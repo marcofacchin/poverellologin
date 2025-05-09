fd5d6ec872746e0757a7d5288fcbce30c425da38
MarcoFacchin <marco.facchin@vdabcampus.be>
2025-05-09 22:49:41 +0200

    aanmeldpagina html gemaakt

---
+++ src/app/pages/create-user/createuser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createuser',
+  imports: [],
+  templateUrl: './createuser.component.html',
+  styleUrl: './createuser.component.css'
+})
+export class CreateuserComponent {
+
+}
