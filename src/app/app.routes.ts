5543fab14e60bd4e3066327b861fd6e059685d28
MarcoFacchin <marco.facchin@vdabcampus.be>
2025-05-09 22:17:04 +0200

    basic routes defined

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './pages/login/login.component';
+import {LayoutComponent} from './pages/layout/layout.component';
+import {UserlistComponent} from './pages/userlist/userlist.component';
+import {CreateuserComponent} from './pages/create-user/createuser.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component:LoginComponent
+  },
+  {
+    path: '',
+    component:LayoutComponent,
+    children: [
+      {
+        path: 'user-list',
+        component:UserlistComponent
+      },
+      {
+        path: 'createUser',
+        component:CreateuserComponent
+      }
+
+    ]
+  }
+];
